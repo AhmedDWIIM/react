0a643534aa5edb7767760ec47a0ce6a5b3625cc7
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-13 11:51:02 +0100

    My todo list with last elmt is red

--- components/Count.js
+++ components/Count.js
@@ -1,19 +1,24 @@
-import react, { useState } from 'react';
+import react, { useState, useEffect } from 'react';
 
 export default function Count() {
+  const [tailleHooks, setTaille] = useState(null);
   const [input, setInput] = useState("");
   const [todolist, setTodolist] = useState([
     "Toto",
     "faire la vaiselle",
   ]);
-
-  function addTodo() {
+    useEffect( () => {
+        localStorage.setItem("todos", todolist)
+        setTaille(todolist.length-1);
+    }, [todolist])
+    
+    function addTodo() {
     setTodolist([...todolist, input]);
   }
 
   function deleteTodo(removeIndex) {
     setTodolist(todolist.filter(function(item, index) { 
-      if (index !== removeIndex) {
+      if (index != removeIndex) {
         return item;
       }
     }));
@@ -27,10 +32,11 @@ export default function Count() {
       { todolist.map((item, index) => {
         return (
           <div key={index}>
-            <p>{index} {item} <span onClick={() => deleteTodo(index)}>Supprimer</span></p>
+            <p style={{color: index === tailleHooks ? "red" : ""}} >{index}. {item} <span onClick={() => deleteTodo(index)}>Supprimer</span></p>
           </div>
         )
       })}
+    
     </div>
   );
 }
