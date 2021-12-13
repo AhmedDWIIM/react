2396bc817af9fffa9c20c877fe212911f45a12ca
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-13 09:49:42 +0100

    Set count +add

--- components/Count.js
+++ components/Count.js
@@ -1,7 +1,12 @@
-import react from 'react'
+import React, { useState } from 'react'
 
-export default function count() {
+export default function Count() {
+    const [count, setCount] = useState(0)
     return  (
-        <div>Count</div>
+        <div>
+            <p>Vous avez clique { count } fois</p> 
+            <br />
+            <button onClick={() => setCount (count+1)}> add 1</button>
+        </div>
     )
 }
