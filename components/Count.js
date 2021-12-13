0fe0f15c076c0019571b2c374b8de0acf1796c40
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-13 09:36:05 +0100

    init Components folder

---
+++ components/Count.js
@@ -0,0 +1,7 @@
+import react from 'react'
+
+export default function count() {
+    return  (
+        <div>Count</div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Count from '../components/Count'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello !</h1>
+        <Count/>
       </main>
 
       <footer className={styles.footer}>
