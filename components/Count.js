6987bf591e7099219ca95736a81bc59c15d5ea91
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-13 10:36:43 +0100

    to do list

--- components/Count.js
+++ components/Count.js
@@ -1,27 +1,36 @@
-import React, { useState } from 'react'
+import react, { useState } from 'react';
 
 export default function Count() {
-    const [todolist, setTodolist] = useState([
-        "toto",
-        "café"
-    ])
-    const [push, setPush] = useState("")
-    
-    return  (
-        <div>
-            <h1> To do :</h1>
-            <form>
-                <label>Ajouter une note</label>
-                <input value={push}   name="push" onChange={e => setPush(e.target.value)} />
-                <button onClick={() => {
-                    setTodolist
-                }}> add note</button>
-            </form>
-            { todolist.map((item, i) => {
-                return (
-                <p key={i}>{item}</p>
-                )
-            })}
-        </div>
-    )
+  const [input, setInput] = useState("");
+  const [todolist, setTodolist] = useState([
+    "Toto",
+    "faire la vaiselle",
+  ]);
+
+  function addTodo() {
+    setTodolist([...todolist, input]);
+  }
+
+  function deleteTodo(removeIndex) {
+    setTodolist(todolist.filter(function(item, index) { 
+      if (index !== removeIndex) {
+        return item;
+      }
+    }));
+  }
+
+  return (
+    <div>
+      <input value={input} onInput={e => setInput(e.target.value)}/>
+      <button onClick={() => addTodo()}>Add</button>
+      <h1>Todo :</h1>
+      { todolist.map((item, index) => {
+        return (
+          <div key={index}>
+            <p>{index} {item} <span onClick={() => deleteTodo(index)}>Supprimer</span></p>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
