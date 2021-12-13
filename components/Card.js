802952ec5426a32f1bcd2f7162f9f5b2582809c3
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-13 15:47:55 +0100

    Card v1

---
+++ components/Card.js
@@ -0,0 +1,14 @@
+import react, { useState, useEffect } from 'react';
+import styles from '../styles/Marketplace.module.css'
+import Image from 'next/image';
+
+export default function Card(props) {
+    return (
+    <div className={styles.card}>
+        <h3>{props.title}</h3><br/>
+        <Image width="100%" height="100%" src={props.img} alt=""/>
+        <p>{props.desc}</p><br/>
+        <h4>{props.price}</h4>
+    </div>
+    );
+}
--- components/Marketplace.js
+++ components/Marketplace.js
@@ -1,11 +1,13 @@
 import react, { useState, useEffect } from 'react';
 import styles from '../styles/Marketplace.module.css'
+import Card from './Card';
 
 export default function Marketplace() {
   const [user, setUser] = useState(null);
   return (
     <div className={styles.navcontainer}>
-      <h3>Marketplace</h3>
+      <h3>Marketplace</h3><br/>
+      <Card title="Piano King king" img="https://storage.opensea.io/static/promocards/rhymeslikedimez-promo.jpeg"desc="THE REALM OF THE PIANO KING HAS NO BORDERS, BECAUSE THE PIANO SPEAKS TO ALL, ACROSS ALL OF TIME." price="4000$"/>
     </div>
   );
 }
