44a77f2485391a0cd162a4952e912cb99b1c18e9
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-14 17:51:57 +0100

    night mode

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,9 +6,16 @@ import loupe from '../public/loupe.png';
 
 export default function Navbar() {
   const [user, setUser] = useState(null);
+
+  const  [colormode, setColormode] = useState("navbar navbar-light");
+  const  [changemode, setChangemode] = useState(false);
+  useEffect( () => {
+    console.log(colormode)
+    changemode ? (setColormode("navbar navbar-dark bg-dark")) : (setColormode("navbar navbar-light"))
+  }, [changemode,colormode]) 
   return (
     <div className={styles.navcontainer}>
-      <nav className="navbar navbar-light">
+      <nav className={colormode}>
         <div className="container-fluid">
          <h3> <a className="navbar-brand"><b>NFT Marketplace</b></a></h3>
           <form className="d-flex">
@@ -22,10 +29,9 @@ export default function Navbar() {
             <button className="btn btn-primary" type="submit">Login</button>
           )}
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                <label className="form-check-label" htmlFor="flexCheckDefault">
-                Night mode
-                </label>
+                <button className="form-check-label" htmlFor="flexCheckDefault" onClick={() => setChangemode(!changemode)} >Night mode </button>
+                
+      
             </div>
         </div>
         </div>
