3e50747fa090e020de840744f42b752dcde38528
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-14 11:53:56 +0100

    Marketplace v4

--- components/Marketplace.js
+++ components/Marketplace.js
@@ -15,10 +15,10 @@ export default function Marketplace() {
           <Card title="Piano King king" img="https://storage.opensea.io/static/promocards/rhymeslikedimez-promo.jpeg"desc="Lorem ipsum dolor sit amet. Ut enim ad minim veniam" price="4000$"/>
           </div>
           <div className={styles.card}>
-          <Card title="Piano King king" img="https://storage.opensea.io/static/promocards/rhymeslikedimez-promo.jpeg"desc="Lorem ipsum dolor sit amet. Ut enim ad minim veniam" price="4000$"/>
+          <Card title="Mariscal" img="https://storage.opensea.io/static/promocards/mariscal-promocard.jpeg"desc="Lorem ipsum dolor sit amet. Ut enim ad minim veniam" price="4000$"/>
           </div>
           <div className={styles.card}>
-          <Card title="Piano King king" img="https://storage.opensea.io/static/promocards/rhymeslikedimez-promo.jpeg"desc="Lorem ipsum dolor sit amet. Ut enim ad minim veniam" price="4000$"/>
+          <Card title="Memory" img="https://storage.opensea.io/static/promocards/memory-promocard.jpeg"desc="Lorem ipsum dolor sit amet. Ut enim ad minim veniam" price="4000$"/>
           </div>
       </div>
     </div>
